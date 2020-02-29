cfa5e23879e49030a617cb545268ceb98c1353cc
Maxime Froment <mfroment99@gmail.com>
2020-02-29 18:24:23 +0900

    Change to compile all scripts and copy all necessary files to a target output directory.

--- packing/compile.bat
+++ packing/compile.bat
@@ -1,1 +1,14 @@
-"C:\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" /in GridMoveP1.ahk /out GridMove.exe /icon Images\GridMove.ico
+set OUTPUTDIR=_out_
+set COMPILER="C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
+
+rmdir /S /Q %OUTPUTDIR%
+mkdir %OUTPUTDIR% %OUTPUTDIR%\Plugins 
+
+%COMPILER% /in GridMove.ahk /out %OUTPUTDIR%\GridMove.exe /icon Images\GridMove.ico
+for %%f in (Plugins\*.ahk) do (
+    %COMPILER% /in %%f /out %OUTPUTDIR%\Plugins\%%~nf.exe /icon Images\GridMove.ico
+  )
+for %%d in (Images Documents Grids) do (
+    xcopy /I /S /E %%d %OUTPUTDIR%\%%d
+  )
+copy packing\GridMoveHelp_*.txt %OUTPUTDIR%\
